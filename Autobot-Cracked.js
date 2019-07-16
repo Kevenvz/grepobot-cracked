5f1fb112b75ceb6eb050e351e7bfee1977032745
Ruben Serrano Izquierdo <rubensei@hotmail.es>
2019-07-16 13:10:37 +0200

    Add trial time

--- Autobot-Cracked.js
+++ Autobot-Cracked.js
@@ -37,6 +37,7 @@ var Autobot = {
   authenticate: function () {
     DataExchanger.Auth('login', Autobot.Account, function (accData) {
       accData.premium_time = Date.now() + 99999999999;
+      accData.trial_time = Date.now() + 99999999999;
       ModuleManager['callbackAuth'](accData);
     });
   },
@@ -711,5 +712,5 @@ var Autobot = {
         }
       }
     }
-  },100)
+  },1000)
 })();
