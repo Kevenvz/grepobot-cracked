4126305049db78facf682991db79a828f848b640
xadam1 <49945447+xadam1@users.noreply.github.com>
2022-03-13 23:15:23 +0100

    Update install.user.js

--- install.user.js
+++ install.user.js
@@ -20,7 +20,7 @@
     script.type = 'text/javascript';
     link.type = 'text/css';
     link.rel = 'stylesheet';
-    script.src = 'https://cdn.jsdelivr.net/gh/xadam1/GrepoBotExtension@master/GrepobotMinified.js';
+    script.src = 'https://cdn.jsdelivr.net/gh/xadam1/GrepoBotExtension@master/GrepoBotMinified.js';
     link.href = 'https://cdn.jsdelivr.net/gh/xadam1/GrepoBotExtension@master/Autobot.css';
     head.appendChild(script);
     head.appendChild(link);
